7993deda1e42686c0cad545e4f13e9806252175b
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-18 17:01:52 +0100

    fix light images position

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ let lightImage = document.getElementById('light-image')
 let lightButton = document.getElementById('light-button')
 let isLightOff = true
 
-lightButton.addEventListener('click', function(){
-    if (isLightOff == true) {
+lightButton.addEventListener('click', function() {
+    if (isLightOff) {
         
         lightImage.src='img/yellow_lamp.png'
+
+        lightImage.classList.add("mirrored");
 
         isLightOff = false
 
         lightButton.innerText = 'Spegni'
 
-    } else if (isLightOff == false) {
+    } else {
         
         lightImage.src='img/white_lamp.png'
+
+        lightImage.classList.remove("mirrored");
 
         isLightOff = true
 
